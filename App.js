7cd88d600ae4b90e302d2ab0eeb0e85a30f9a861
serhatturkmen <serhat_21_88@hotmail.com>
2021-08-19 16:23:06 +0300

    Config Router File

--- App.js
+++ App.js
@@ -1,15 +1,6 @@
 import React from "react";
 import Router from "./src/Router";
 
-import HomeStackNavigation from "./src/navigation/HomeStackNavigation";
-
-import { getStatusBarHeight } from "react-native-status-bar-height";
-import { NavigationContainer } from "@react-navigation/native";
-
 export default function App() {
-  return (
-    <NavigationContainer style={{ marginTop: getStatusBarHeight() }}>
-      <HomeStackNavigation />
-    </NavigationContainer>
-  );
+  return <Router />;
 }
--- src/Router.js
+++ src/Router.js
@@ -1,37 +1,12 @@
 import React from "react";
-import { View, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
-const BottomTab = createBottomTabNavigator();
-
-const TestComp = (props) => {
-  return (
-    <View>
-      <Text>{props.route.screenName}</Text>
-    </View>
-  );
-};
+import HomeStackNavigation from "./navigation/HomeStackNavigation";
+import { getStatusBarHeight } from "react-native-status-bar-height";
 
 const Router = () => {
   return (
-    <NavigationContainer>
-      <BottomTab.Navigator>
-        <BottomTab.Screen
-          name="test1"
-          component={TestComp}
-        />
-        <BottomTab.Screen
-          name="test2"
-          component={TestComp}
-          options={{ title: "null" }}
-        />
-        <BottomTab.Screen
-          name="test3"
-          component={TestComp}
-          options={{ title: "Main" }}
-        />
-      </BottomTab.Navigator>
+    <NavigationContainer style={{ marginTop: getStatusBarHeight() }}>
+      <HomeStackNavigation />
     </NavigationContainer>
   );
 };
